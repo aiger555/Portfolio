6b2f75ddcf511290b83074f0d3839fc724e6d3fb
Aigerim Nuralieva <75246512+aiger555@users.noreply.github.com>
2021-04-21 18:04:19 +0600

    Add files via upload

---
+++ portfolio.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<head>
+  <link rel="stylesheet" href="style.css">
+    <title class="d">Aigerim's Portfolio</title>
+    <style>
+        .leftpic {
+         float: left; 
+         margin: 1 1 6px 6px; 
+        }
+       </style>
+ <body>
+   <div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIeittjp16URuJ2pCevZ5Fg-AI_vRuL1NbMw&usqp=CAU"  width="200" height="272" class="leftpic"> <h4>Name: Aigerim</h4> <h4>Surname: Nuralieva</h4> </div>
+   <h4> Description: I study at Ala-Too university </h4>
+   <h3 class="txt">Contacts: </h3>
+   <footer>
+     <ul>
+       <li><p>Whatsapp: 0700842474</p></li>
+       <li><p>Gmail: aigerim.nuralieva@iaau.edu.kg</p></li>
+     </ul>
+   </footer>
+ </body>
+ 
+</head>
